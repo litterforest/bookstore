db7b75e22cce190a05f14b8b7cbf4d4f10d54fb4
litterforest <18027041863@163.com>
2017-12-25 12:01:16 +0800

    增加商品抢购功能

--- src/main/java/com/cobee/bookstore/component/listener/AuctionListener.java
+++ src/main/java/com/cobee/bookstore/component/listener/AuctionListener.java
@@ -1,14 +1,22 @@
 package com.cobee.bookstore.component.listener;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Application Lifecycle Listener implementation class AuctionListener
@@ -31,25 +39,47 @@ public class AuctionListener implements ServletContextListener {
      * Default constructor. 
      */
     public AuctionListener() {
-        // TODO Auto-generated constructor stub
     }
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		logger.info("秒杀抢购功能已起动...");
 		thread = new Thread(new Runnable() {
+			
+			private ObjectMapper mapper = new ObjectMapper();
 			
 			@Override
 			public void run() {
 				while(true)
 				{
 					AsyncContext asyncContext = null;
-					if (queue.peek() != null)
+					while (queue.peek() != null)
 					{
 						try {
 							asyncContext = queue.poll();
+							ServletRequest request = asyncContext.getRequest();
+							ServletResponse response = asyncContext.getResponse();
+							response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+							PrintWriter writer = response.getWriter();
+							Thread.sleep(1000);
+							String userno = request.getParameter("userno");
+							String threadName = Thread.currentThread().getName();
+							// 模拟计算时间
+							long startTime = System.currentTimeMillis();
 							
+							long endTime = System.currentTimeMillis();
+							String duration = String.valueOf((endTime - startTime) / 1000);
 							
+							Map<String, Object> map = new HashMap<String, Object>();
+							map.put("status", "success");
+							map.put("userno", userno);
+							map.put("threadName", threadName);
+							map.put("duration", duration);
+							String jsonStr = mapper.writeValueAsString(map);
 							
+							writer.println(jsonStr);
+							
+							writer.close();
 						} catch (Exception e) {
 							logger.error("", e);
 						} 
